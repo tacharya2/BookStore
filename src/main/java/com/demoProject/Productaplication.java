5a4d36cdd51b3d5a73c912db025321689e780d88
tacharya2 <akash_sharma430@yahoo.com>
2023-06-02 10:55:00 -0400

    First commit

---
+++ src/main/java/com/demoProject/Productaplication.java
@@ -0,0 +1,15 @@
+package com.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Productaplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productaplication.class, args);
+
+	}
+}
+
---
+++ src/main/java/com/mapper/ProductMapper.java
@@ -0,0 +1,6 @@
+package com.mapper;
+
+
+public class ProductMapper {
+
+}
